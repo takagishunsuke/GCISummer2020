6a1d327be8cc1323ae1dd672a7d4aa828db26884
た か ぎ <takagishunsuke1129@gmail.com>
2020-05-16 16:44:22 +0900

    Add files via upload

---
+++ roop_test.py
@@ -0,0 +1,17 @@
+x = 0
+n = 1
+
+#When "n" is under 10
+while (n <= 10):
+    #"x" change to "x + n"
+    x = x + n
+
+    #"n" change to "n + 1"
+    n = n + 1
+
+    #it's roop so, will be add 9 times
+    #it mean 1+2+3+4+5+6+7+8+9+10
+print(x)
+
+nomarl = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10
+print(nomarl)
